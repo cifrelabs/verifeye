4d8fc1e28fff933f4509d5994d2fce8458d276ce
cifelse <meetcif@gmail.com>
2024-03-26 20:08:38 +0800

    Add a loading screen

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center h-screen bg-black" >
+            <img src="/images/verifeye.png" alt="Verifeye Logo" />
+        </div>
+    );
+};
+
+export default Loading;
