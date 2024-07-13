472dc352b055c15e582b15d173633f49ce3cbc0c
cifelse <meetcif@gmail.com>
2024-07-13 22:09:53 +0800

    Add buttons

--- src/app/components/Highlight.tsx
+++ src/app/components/Highlight.tsx
@@ -55,23 +55,19 @@ const Content: React.FC<HighlightProps> = async ({
 
     // No Social Media
     return (
-        <div className="h-screen top-24 justify-center relative px-5">
-            <div className="flex flex-col items-center text-center gap-5 z-10">
-                <div className='flex flex-col items-center'>
-                    <img 
-                        src={pfp} 
-                        alt={`${displayName}'s profile picture`}
-                        className="w-24 h-24 rounded-full mb-2"
-                    />
-                    <h2 className="text-lg font-bold">{displayName}</h2>
-                    <p className="text-sm">@{username}</p>
-                </div>
-                <div>
-                    <p className="text-base font-medium text-pretty">
-                        No possible matching accounts nor mentions were found
-                    </p>
-                </div>
-                <div className='flex flex-col w-full gap-2'>
+        <div className="h-screen flex items-center justify-center relative">
+            <div className="flex flex-col items-center text-center z-10">
+                <img 
+                    src={pfp} 
+                    alt={`${displayName}'s profile picture`}
+                    className="w-24 h-24 rounded-full mb-4"
+                />
+                <h2 className="text-xl font-bold">{displayName}</h2>
+                <p className="text-sm text-gray-600 mb-6">@{username}</p>
+                <p className="text-lg pl-10 pr-10 mb-6">
+                    No possible matching accounts nor mentions were found
+                </p>
+                <div className='flex flex-col w-full gap-2 px-5'>
                     <button className='w-full bg-tiktok-red rounded-md py-2 px-3'>
                         <p className="text-kinda-sm tracking-wide">See what else we found about this user</p>
                     </button>
@@ -81,6 +77,26 @@ const Content: React.FC<HighlightProps> = async ({
                 </div>
             </div>
         </div>
+
+        // <div className="h-screen top-24 justify-center relative px-5">
+        //     <div className="flex flex-col items-center text-center gap-5 z-10">
+        //         <div className='flex flex-col items-center'>
+        //             <img 
+        //                 src={pfp} 
+        //                 alt={`${displayName}'s profile picture`}
+        //                 className="w-24 h-24 rounded-full mb-2"
+        //             />
+        //             <h2 className="text-lg font-bold">{displayName}</h2>
+        //             <p className="text-sm">@{username}</p>
+        //         </div>
+        //         <div>
+        //             <p className="text-base font-medium text-pretty">
+        //                 No possible matching accounts nor mentions were found
+        //             </p>
+        //         </div>
+                
+        //     </div>
+        // </div>
     );
 };
 
