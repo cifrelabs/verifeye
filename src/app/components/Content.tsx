53f8ddff7f6873adf0425f385c3113be100500bf
cifelse <meetcif@gmail.com>
2024-07-13 22:02:36 +0800

    Add padding right for captions

--- src/app/components/Content.tsx
+++ src/app/components/Content.tsx
@@ -122,7 +122,7 @@ const Content: React.FC<ContentProps> = ({
             )}
             <div className="absolute bottom-12 pb-3 left-4 text-white">
                 <h2 className="text-lg font-bold">{displayName}</h2>
-                <p className="text-sm mt-1">{captions}</p>
+                <p className="text-sm mt-1 pr-11 mr-3">{captions}</p>
                 <p className="text-xs mt-2 flex items-center">
                     <svg className="w-4 h-4 mr-1" fill="currentColor" viewBox="0 0 24 24">
                         <path d="M12 3v10.55c-.59-.34-1.27-.55-2-.55-2.21 0-4 1.79-4 4s1.79 4 4 4 4-1.79 4-4V7h4V3h-6z"/>
