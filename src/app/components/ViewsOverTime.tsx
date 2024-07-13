08ea04ef0f383ee8ceac3ea655e5d9f611dbe830
RalphM-02 <ralph_marquez@dlsu.edu.ph>
2024-07-13 16:26:58 +0800

    fix: top nav

--- src/app/components/ViewsOverTime.tsx
+++ src/app/components/ViewsOverTime.tsx
@@ -152,22 +152,6 @@ const ViewsOverTime: React.FC = () => {
         return { dates, views, counts };
     };
 
-    // const abbreviateNumber = (value: number) => {
-    //     let newValue = value;
-    //     if (value >= 1000) {
-    //         const suffixes = ["", "K", "M", "B", "T"];
-    //         const suffixNum = Math.floor(("" + value).length / 3);
-    //         let shortValue = '';
-    //         for (let precision = 2; precision >= 1; precision--) {
-    //             shortValue = parseFloat((suffixNum !== 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision));
-    //             let dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
-    //             if (dotLessShortValue.length <= 2) { break; }
-    //         }
-    //         newValue = shortValue + suffixes[suffixNum];
-    //     }
-    //     return newValue;
-    // };
-
     const abbreviateNumber = (value: number) => {
         if (value < 1000) {
             return value.toString();
